bdcd6ece1771e188be1eb4056d66d50c6837c8a2
bulutcakan <bcakan@migros.com.tr>
2020-04-19 15:13:23 +0300

    bing bang

---
+++ src/main/java/com/bulut/inceptorexample/InceptorexampleApplication.java
@@ -0,0 +1,13 @@
+package com.bulut.inceptorexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InceptorexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InceptorexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bulut/inceptorexample/InceptorexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulut.inceptorexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InceptorexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
